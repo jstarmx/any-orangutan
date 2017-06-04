4c7320b88b8e375030695ae25db3627d3a6fd768
James Starkie <james.starkie@onthebeach.co.uk>
2017-06-04 14:08:40 +0100

    App entry file test

--- app/scripts/__tests__/app.test.jsx
+++ app/scripts/__tests__/app.test.jsx
@@ -1,11 +1,38 @@
 import { install } from 'offline-plugin/runtime';
+import React from 'react';
+import { render } from 'react-dom';
+import * as redux from 'redux';
+import { Provider } from 'react-redux';
+
+import Gallery from '../containers/gallery';
+import reducer from '../reducers/gallery';
 
 jest.mock('offline-plugin/runtime');
+jest.mock('react-dom');
+jest.mock('redux');
+jest.mock('redux-thunk', () => 'thunk');
 
-// eslint-disable-next-line global-require
-const subject = () => require('../app.jsx');
+document.body.innerHTML = '<div class="gallery-container"></div>';
+window.__REDUX_DEVTOOLS_EXTENSION__ = () => 'devtools';
+redux.applyMiddleware = middleware => middleware;
+redux.createStore = jest.fn();
+const store = redux.createStore();
+
+require('../app');
 
 it('calls the offline-plugin install script', () => {
-  subject();
   expect(install).toBeCalled();
+});
+
+it('creates a store', () => {
+  expect(redux.createStore).toBeCalledWith(reducer, 'devtools', 'thunk');
+});
+
+it('renders the Gallery container', () => {
+  expect(render).toBeCalledWith(
+    <Provider store={ store }>
+      <Gallery />
+    </Provider>,
+    document.querySelector('.gallery-container')
+  );
 });
